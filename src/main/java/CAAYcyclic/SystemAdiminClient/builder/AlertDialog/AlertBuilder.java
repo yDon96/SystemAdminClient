63efdcccbd7323279bc59574a3d2c016f4e20254
Youssef <you.donadeo@icloud.com>
2020-12-07 23:55:02 +0100

    Create builder to construct Allert message box

---
+++ src/main/java/CAAYcyclic/SystemAdiminClient/builder/AlertDialog/AlertBuilder.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CAAYcyclic.SystemAdiminClient.builder.AlertDialog;
+
+import java.awt.event.MouseAdapter;
+
+/**
+ *
+ * @author Youssef
+ */
+public interface AlertBuilder {
+    
+     /**
+     * Set Title to display in Alert Panel.
+     * @param title
+     */
+    void setTitle(String title);
+    
+     /**
+     * Set Message to display in Alert Panel.
+     * @param message
+     */
+    void setMessage(String message);
+    
+     /**
+     * Set Positive Button to display in Alert Panel.
+     * @param value Text to display.
+     * @param action Action to perform.
+     */
+    void setPositiveButton(String value,MouseAdapter action);
+    
+     /**
+     * Set Negative Button to display in Alert Panel.
+     * @param value Text to display.
+     * @param action Action to perform.
+     */
+    void setNegativeButton(String value,MouseAdapter action);
+}
---
+++ src/main/java/CAAYcyclic/SystemAdiminClient/builder/AlertDialog/impl/AlertDialogBuilder.java
@@ -0,0 +1,178 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CAAYcyclic.SystemAdiminClient.builder.AlertDialog;
+
+import CAAYcyclic.SystemAdiminClient.builder.AlertDialog.AlertBuilder;
+import CAAYcyclic.SystemAdiminClient.controller.frame.MainFrameController;
+import CAAYcyclic.SystemAdiminClient.navigation.NavigationController;
+import CAAYcyclic.SystemAdiminClient.view.frame.AlertJDialog;
+import CAAYcyclic.SystemAdiminClient.view.panel.content.AlertPanel;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.logging.Logger;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Youssef
+ */
+public class AlertDialogBuilder implements AlertBuilder {
+    
+    private String title;
+    private String message;
+    private String positiveTitle;
+    private String negativeTitle;
+    private MouseAdapter positiveAction;
+    private MouseAdapter negativeAction;
+    private AlertJDialog dialog;
+    
+    private static final Logger LOG = Logger.getLogger(AlertDialogBuilder.class.getName());
+
+    /**
+     * Set title or will be display the default one.
+     */
+    @Override
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * Set message or will be display the default one (Empty string).
+     */
+    @Override
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    /**
+     * If not set value and MouseAdapter, it will be removed from panel.
+     * If value will not be set, default value will be assigned.
+     * If MouseAdapter will not be set, default MouseAdapter will be assigned(Close Dialog).
+     */
+    @Override
+    public void setPositiveButton(String value, MouseAdapter action) {
+        this.positiveTitle = value;
+        this.positiveAction = action;
+    }
+
+    /**
+     * If not set value and MouseAdapter, it will be removed from panel.
+     * If value will not be set, default value will be assigned.
+     * If MouseAdapter will not be set, default MouseAdapter will be assigned(Close Dialog).
+     */
+    @Override
+    public void setNegativeButton(String value, MouseAdapter action) {
+        this.negativeTitle = value;
+        this.negativeAction = action;
+    }
+    
+    public void show() {
+        MainFrameController mainFrameController = NavigationController.getInstance().getFrameController();
+        if(mainFrameController == null){
+            LOG.log(java.util.logging.Level.SEVERE, "Dialog parent frame not exist.");
+            return;
+        }
+        dialog = new AlertJDialog(mainFrameController.getMainFrame(), true);
+        dialog.setContentPane(getAlertPanel());
+        dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+        dialog.setVisible(true);
+        LOG.log(java.util.logging.Level.WARNING, "Showing Dialog.");
+    }
+    
+    public void dispose() {
+        if(dialog == null){
+            LOG.log(java.util.logging.Level.SEVERE, "The Dialog to dispose not exist.");
+            return;
+        }
+        dialog.dispose();
+    }
+    
+    private JPanel getAlertPanel() {
+        AlertPanel alertPanel = new AlertPanel();
+        customizeTitle(alertPanel);
+        customizeMessage(alertPanel);
+        customizeNegativeBtn(alertPanel);
+        customizePositiveBtn(alertPanel);
+        alertPanel.refresh();
+        return alertPanel;
+    }
+    
+    private void customizeTitle(AlertPanel alertPanel){
+        if(title != null){
+            alertPanel.getTitleDlg().setText(title);
+        }
+    }
+    
+    private void customizeMessage(AlertPanel alertPanel){
+        if(message != null){
+            alertPanel.getMessageDlg().setText(message);
+        } else {
+            alertPanel.getMessageDlg().setText("");
+        }
+    }
+    
+    private void customizePositiveBtn(AlertPanel alertPanel){
+        JButton positiveButton = alertPanel.getPositiveBtn();
+        if(positiveTitle == null && positiveAction == null){
+            positiveButton.setVisible(false);
+            return;
+        }
+        
+        if(positiveTitle != null){
+            positiveButton.setText(positiveTitle);
+        } else {
+           positiveButton.setText("Ok");
+        }
+        
+        if(positiveAction != null){
+            positiveButton.addMouseListener(closeDialogAction);
+            positiveButton.addMouseListener(positiveAction);
+        } else {
+            positiveButton.addMouseListener(closeDialogAction);
+        }
+    }
+    
+    private void customizeNegativeBtn(AlertPanel alertPanel){
+        JButton negativeButton = alertPanel.getNegativeBtn();
+        if(negativeTitle == null && negativeAction == null){
+            negativeButton.setVisible(false);
+            return;
+        }
+        if(negativeTitle!= null){
+            negativeButton.setText(negativeTitle);
+        } else {
+           negativeButton.setText("Cancel");
+        }
+        
+        if(negativeAction != null){
+            negativeButton.addMouseListener(closeDialogAction);
+            negativeButton.addMouseListener(negativeAction);
+        } else {
+            negativeButton.addMouseListener(closeDialogAction);
+        }
+    }
+    
+    public void setDefaultPositiveAction(){
+        positiveAction = closeDialogAction;
+    }
+    
+    public void setDefaultNegativeAction(){
+        negativeAction = closeDialogAction;
+    }
+    
+    private final MouseAdapter closeDialogAction = new MouseAdapter() {
+        @Override
+        public void mousePressed(MouseEvent mouseEvent) {
+            super.mousePressed(mouseEvent);
+            dispose();
+        }
+    };
+    
+}
