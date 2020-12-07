5fece72eb62b639ec0523c0bd238d390bdaeab62
Youssef <you.donadeo@icloud.com>
2020-12-07 23:43:00 +0100

    Create a back bar panel

---
+++ src/main/java/CAAYcyclic/SystemAdiminClient/controller/bar/BackBarController.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CAAYcyclic.SystemAdiminClient.controller.bar;
+
+import CAAYcyclic.SystemAdiminClient.navigation.Segue;
+import CAAYcyclic.SystemAdiminClient.view.panel.bar.BackSideBarPanel;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Youssef
+ */
+public class BackBarController extends BarController {
+
+    private BackSideBarPanel mainSideBarView;
+    
+    private static final Logger LOG = Logger.getLogger(BackBarController.class.getName());
+
+    public BackBarController() {
+        super();
+        setBarPanel(BackSideBarPanel.class);
+    }
+    
+    @Override
+    public void panelDidAppear() {
+        super.panelDidAppear();
+        this.mainSideBarView = (BackSideBarPanel) getPanel();
+         mainSideBarView.getBackBtn().addMouseListener(backBtnAction);
+    }
+
+     
+    private MouseAdapter backBtnAction = new MouseAdapter() {
+        @Override
+        public void mousePressed(MouseEvent mouseEvent) {
+            super.mousePressed(mouseEvent);
+            backAction();
+        }
+    };
+    
+    private void backAction() {
+        if(!isLockNavigation()){
+            popBackView();
+        } else {
+            LOG.log(java.util.logging.Level.WARNING, "Cannot popBack, navigation is locked.");
+        }
+    }
+
+    @Override
+    public Logger getLogger() {
+        return LOG;
+    }
+
+    @Override
+    public void prepare(Segue segue) {
+    }
+}
