b1cdb72381670303e8532e04cd452769bc33928f
Youssef <you.donadeo@icloud.com>
2020-12-07 23:38:10 +0100

    Create DashBoard

---
+++ src/main/java/CAAYcyclic/SystemAdiminClient/controller/content/DashBoardPanelController.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CAAYcyclic.SystemAdiminClient.controller.content;
+
+import CAAYcyclic.SystemAdiminClient.navigation.Segue;
+import CAAYcyclic.SystemAdiminClient.view.panel.content.DashBoardPanel;
+import java.util.logging.Logger;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author Youssef
+ */
+public class DashBoardPanelController extends ContentPanelController {
+
+    private static final Logger LOG = Logger.getLogger(DashBoardPanelController.class.getName());
+
+    public DashBoardPanelController() {
+        super();
+        setContentPanel(DashBoardPanel.class);
+    }
+
+    @Override
+    public void panelWillAppear() {
+        super.panelWillAppear();
+    }
+
+    @Override
+    public Logger getLogger() {
+        return LOG;
+    }
+
+    @Override
+    public void prepare(Segue segue) {
+    }
+
+    
+}
