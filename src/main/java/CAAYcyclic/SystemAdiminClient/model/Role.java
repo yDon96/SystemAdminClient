ef90e5cf40459ade732fd04daefd82507b0255f8
Youssef <you.donadeo@icloud.com>
2020-12-13 11:48:32 +0100

    Define Role object

---
+++ src/main/java/CAAYcyclic/SystemAdiminClient/model/Role.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CAAYcyclic.SystemAdiminClient.model;
+
+/**
+ *
+ * @author Youssef
+ */
+public class Role {
+    
+    Integer id;
+    String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+}
